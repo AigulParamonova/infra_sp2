456698d1a737318d527e96eadf4b946a39597b1b
AigulParamonova <aigul.paramonova@yandex.ru>
2021-11-14 09:13:53 +0300

    Проект 12 спринта

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
